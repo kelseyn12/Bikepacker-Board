93375fc589db8a214ddf3b456d46ec6a5f983cfa
kelsey nocek <kelseyn12@hotmail.com>
2022-01-07 16:42:44 -0600

    added dependencies to routes controller

---
+++ controllers/routesCtr.js
@@ -0,0 +1,35 @@
+/*========================
+        Dependencies
+==========================*/
+const express = require("express");
+const routesRouter = express.Router();
+const Route = require('../models/routes');
+const routeSeed = require('../models/routeSeed');
+
+/*======================
+        Routes
+========================*/
+
+//SEED//
+
+
+
+//INDEX//
+
+//NEW//
+
+//DELETE//
+
+//UPDATE//
+
+
+//CREATE//
+
+//EDIT//
+
+//SHOW//
+
+
+
+
+module.exports = routesRouter;
