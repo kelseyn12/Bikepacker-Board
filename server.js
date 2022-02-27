5e53344faf782b67de209f0b88ce55245daa4fe2
kelsey nocek <kelseyn12@hotmail.com>
2022-02-27 15:20:13 -0600

    re-add bikepacking routes

--- server.js
+++ server.js
@@ -8,24 +8,27 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const routesController = require('./controllers/routesCtr');
 
-require('dotenv'). config();
+require('dotenv').config();
 
 /*==============================
         Database connection
 ==============================*/
-const { PORT, DATABASE_URL } = process.env;
+const {
+        PORT,
+        DATABASE_URL
+} = process.env;
 mongoose.connect(DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
 });
 
 const db = mongoose.connection
 db.on('error', (error) =>
-console.log(error.message + ' is mongo not running?'));
+        console.log(error.message + ' is mongo not running?'));
 db.on('connected', () =>
-console.log('mongo connected'));
+        console.log('mongo connected'));
 db.on('disconnected', () =>
-console.log('mongo disconnected'));
+        console.log('mongo disconnected'));
 
 /*================================
         Middleware
@@ -33,7 +36,9 @@ console.log('mongo disconnected'));
 app.use(morgan('dev'));
 
 //body parser middleware: give access to req.body
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+        extended: false
+}));
 
 app.use(methodOverride("_method"));
 app.use('/public', express.static('public'));
@@ -41,7 +46,7 @@ app.use("/routes/", routesController);
 
 //ROUTES//
 app.get('/', (req, res) => {
-    res.redirect('/routes')
+        res.redirect('/routes')
 });
 
 app.use('/routes', routesController);
@@ -49,12 +54,12 @@ app.use('/routes', routesController);
 /*=============================
         Catch URL Mistakes
 =============================*/
-app.get("/", (req,res) => {
-	res.redirect("/products");
+app.get("/", (req, res) => {
+        res.redirect("/products");
 });
 
-app.get("/*", (req,res) => {
-	res.send("<h1>404 - Page Not Found</h1>");
+app.get("/*", (req, res) => {
+        res.send("<h1>404 - Page Not Found</h1>");
 });
 
 
@@ -62,4 +67,4 @@ app.get("/*", (req,res) => {
         Listeners
 =============================*/
 app.listen(PORT, () =>
-console.log(`server is listening on port: ${PORT}`));
+        console.log(`server is listening on port: ${PORT}`));
